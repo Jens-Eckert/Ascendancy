9206f227ddb2703812e479f077a258230d20e04e
Jens-Eckert <capt.lupa@gmail.com>
2024-07-24 16:46:21 -0400

    basic movement

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,69 +1,72 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Test : MonoBehaviour, InputSystem_Actions.IPlayerActions
-{
+public class PlayerController : MonoBehaviour {
     private InputSystem_Actions m_actions;
+    private CharacterController m_characterController;
+    private Vector3 m_velocity = Vector3.zero;
+    [SerializeField] private float accelerationTime;
+    [SerializeField] private float decelerationTime;
+    [SerializeField] private float speed;
+    [SerializeField] private float maxLookAngle;
+    private Transform m_lookAt;
+    private Vector2 m_rv;
+    private float m_lookAngle = 0.0f;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    private void Start() {
         m_actions ??= new InputSystem_Actions();
-        m_actions.Player.SetCallbacks(this);
 
+        m_actions.Player.Look.performed += _ => OnLook();
+        
         m_actions.UI.Disable();
         m_actions.Player.Enable();
+        
+        m_characterController = gameObject.GetComponent<CharacterController>();
+
+        m_lookAt = transform.Find("LookAt");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Awake() {
         
     }
+    
+    private void Update() {
+        OnMove(m_actions.Player.Move.ReadValue<Vector2>());
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        Debug.Log("Move " + context.action.name);
+        var move = transform.right * m_velocity.x + transform.forward * m_velocity.z;
+
+        m_characterController.Move(move * (Time.deltaTime * speed));
     }
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
+    private void OnMove(Vector2 input) {
+        if (input != Vector2.zero) {
+            m_velocity.x = Mathf.SmoothDamp(m_velocity.x, input.x, ref m_rv.x, accelerationTime);
+            m_velocity.z = Mathf.SmoothDamp(m_velocity.z, input.y, ref m_rv.y, accelerationTime);
+        } else {
+            m_velocity.x = Mathf.SmoothDamp(m_velocity.x, input.x, ref m_rv.x, decelerationTime);
+            m_velocity.z = Mathf.SmoothDamp(m_velocity.z, input.y, ref m_rv.y, decelerationTime);
+        }
     }
 
-    public void OnAttack(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
+    private void OnLook() {
+        var input = m_actions.Player.Look.ReadValue<Vector2>();
+        
+        transform.Rotate(Vector3.up, input.x);
+        m_lookAngle += input.y;
+        m_lookAngle = Mathf.Clamp(m_lookAngle, -maxLookAngle, maxLookAngle);
+        
+        // Debug.Log(m_lookAngle);
+
+        if (m_lookAngle <= -maxLookAngle || m_lookAngle >= maxLookAngle)
+            return;
+        
+        m_lookAt.RotateAround(transform.position, transform.right, -input.y);
     }
 
-    public void OnInteract(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
+    private Vector3 ClampV3(Vector3 v, float min, float max) {
+        v.x = Mathf.Clamp(v.x, min, max);
+        v.y = Mathf.Clamp(v.y, min, max);
+        v.z = Mathf.Clamp(v.z, min, max);
 
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnPrevious(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnNext(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void OnSprint(InputAction.CallbackContext context)
-    {
-        throw new System.NotImplementedException();
+        return v;
     }
 }
